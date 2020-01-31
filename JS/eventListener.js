f9c54029e061eb3c0ae9e9e16c3379b6800b8ad3
Santiago <sagudecod97@gmail.com>
2020-01-31 16:14:14 -0500

    Created event listen to state

---
+++ JS/eventListener.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/*window.addEventListener("load", ()=>{
+    let stateClick = document.getElementById("state")
+    stateClick.addEventListener("click", () => {
+        let state = document.getElementById("state").value
+        console.log(state)
+    })
+    
+    let numSick = document.getElementById("num_sick")
+    numSick.addEventListener("input", (input)=>{
+        console.log(input.target.value)
+    })
+
+})*/
