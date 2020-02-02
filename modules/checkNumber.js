4435e304b55d882bcf08acfe032f8be84fb5b931
Santiago <sagudecod97@gmail.com>
2020-02-02 16:39:44 -0500

    Fixed the sumArr function so it sums correctly when the number is repetitive as '3333'

--- modules/checkNumber.js
+++ modules/checkNumber.js
@@ -1,12 +1,22 @@
-const sumArr = (arr) => {
-    let arr = [ 10, 1000, 3, 100, 4 ]
+const sumArr = (arrCheck) => {
+    var arr = arrCheck
     var arr_sum = 0
 
     for (var i = 0; i < arr.length; i++){
-        if (arr[i] < arr[i + 1]){
+    
+        if (arr[i + 1] == 1000 && arr[i].toString().length < 2){
+            var holder = arr[i]
+            arr[i + 1] = holder * 1000
+            arr[i] = 0
+        } else if (arr[i].toString().length >= 4){
+            continue
+        } else if ((arr[i] < arr[i + 1])){
             var holder = arr[i]
             arr[i] = 0
             arr[i + 1] = holder * arr[i + 1]
+        } else if ((arr[i] > arr[i + 1])){
+            arr[i + 1] = arr[i + 1] + arr[i]
+            arr[i] = 0
         }
     }
     for (var j = 0; j < arr.length; j++){
@@ -15,20 +25,18 @@ const sumArr = (arr) => {
     return arr_sum;
 };
  
-export const checkNumber = (str) => {
+const checkNumber = (str) => {
     if (!isNaN(parseInt(str))){
         return 1
     }
     units_dict = {
+        "fifty": 50, "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90, "hundred": 100,
         "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, 
         "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, 
         "ten": 10,  "eleven": 11, "twelve": 12, "thirteen": 13, 
         "fourteen": 14, "fifteen": 15, "sixteen": 16, 
-        "seventeen": 17, "eighteen": 18, "nineteen": 19,
-        "twenty": 20, "thirty": 30, "forty": 40, "fifty": 50, 
-        "sixty": 60, "seventy": 70, "eighty": 80, 
-        "ninety": 90, "hundred": 100, "thousand": 1000,
-        "million": 1000000
+        "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20,
+        "thousand": 1000, "million": 1000000
       }
     
     let len_str = str.length
@@ -46,10 +54,10 @@ export const checkNumber = (str) => {
                 arr_number.push(units_dict[key])
                 str = str.slice(key.length, str.length)
                 length_iteration += key.length
+                break
             }
         };
     };
-
     let returnNumber = sumArr(arr_number);
     return (returnNumber)
 };
